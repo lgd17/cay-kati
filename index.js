db61b09c925a6c310660f5f09ad0ad00e9a9d518
lgd17 <kblgd892@gmail.com>
2025-07-28 15:30:05 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const menu = {
+  reply_markup: {
+    keyboard: [
+      ['valider', 'coupons'],
+      ['voire les coupon', ' message fixe'],
+      ['voire messagefixe',' message simple'],
+      ['voire messageSimple','envoier message today'],
+      ['rebour'],
+    ],
+    resize_keyboard: true,
+    one_time_keyboard: false
+  }
+};
