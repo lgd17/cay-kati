73e32759d1181b422965df6e6197290518c9c3a0
lgd17 <kblgd892@gmail.com>
2025-09-10 09:45:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ require('./cleanLogs'); // ⬅️ S'il est dans cleanLogs.js
 process.env.TZ = 'Africa/Lome';
 const moment = require('moment-timezone');
 
+
 const ADMIN_ID = process.env.ADMIN_ID;
 const ADMIN_IDS = process.env.ADMIN_IDS.split(",").map(Number);
  
@@ -169,10 +170,8 @@ bot.onText(/\/getid/, async (msg) => {
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
-//=== COMMANDE /ajouter_prono ===
+                   //=== COMMANDE /ajouter_prono ===\\
 // ====================== AJOUT MANUEL DE PRONO ======================
-
-const pendingCoupon = {}; // Stocke l'état du prono pour chaque admin
 
 // Commande pour démarrer l'ajout
 bot.onText(/\/ajouter_prono/, (msg) => {
