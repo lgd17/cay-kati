e957d9f7f6fc4be1c728b5dad66738009bf62a91
lgd17 <kblgd892@gmail.com>
2025-09-11 11:04:05 +0000

    Update sendFixedMessagesDaily.js

--- sendFixedMessagesDaily.js
+++ sendFixedMessagesDaily.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { Pool } = require('pg'); 
+const { pool } = require("./db"); 
 const moment = require("moment-timezone");
 
 async function sendFixedMessagesDaily() {
